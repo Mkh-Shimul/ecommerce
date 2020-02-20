f74c8ae6d1b699741bfc7e80bad6c5ca1580ed0e
Mkh-Shimul <mkhshimul7@gmail.com>
2020-02-21 00:38:15 +0600

    First setup

---
+++ partials/_dash_footer.php
@@ -0,0 +1,8 @@
+<script src="node_modules/jquery/dist/jquery.slim.min.js"></script>
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
+<script src="node_modules/feather-icons/dist/feather.min.js"></script>
+<script>
+    feather.replace();
+</script>
+</body>
+</html>
---
+++ partials/_footer.php
@@ -0,0 +1,12 @@
+<footer class="text-muted">
+    <div class="container">
+        <p class="float-right">
+            <a href="#">Back to top</a>
+        </p>
+        <p>&copy; Mkh-Shimul <?php echo Date('Y'); ?></p>
+    </div>
+</footer>
+<script src="node_modules/jquery/dist/jquery.slim.min.js"></script>
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
+</body>
+</html>
